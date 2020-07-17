1c0cfe49832349df10388636bbbf9ecc8bdf70de
Sanity.io <hello@sanity.io>
2020-07-17 23:00:54 +0800

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# yatpd-blog-studio
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 't1i6133u',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
